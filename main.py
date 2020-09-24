33800963c4c7b03d7dc333d5f103d701bf7d4364
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-09-24 14:54:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,31 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def read_message():
+# Read the message in entirety
+	# Message = “”
+	# Lines = read_header_line_count()
+	# While lines > 0:
+	# 	Message += Read_line()
+	# 	Lines -= 1
+	# Return message # or print() depending  on what we should be doing
+
+def read_header_line_count():
+# returns the line count of the message
+	
+def read_line():
+#invokes a while loop, constantly adding the next byte to a bytes literal until a newline char is parsed.
+	# Line = b’’
+	# New_char = b‘’
+	# While not new_char == b’\n’:
+	# 	New_char = next_byte()
+
+def next_byte():
+#reads and returns the next byte
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
